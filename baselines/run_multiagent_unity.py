a3ba5bf8848dc3c7e0ef40dde00156d9cf849f9c
Joe Booth <joe@joebooth.com>
2018-11-08 19:10:20 -0800

    use mujoco settings for training unity

--- baselines/run_multiagent_unity.py
+++ baselines/run_multiagent_unity.py
@@ -59,7 +59,7 @@ def train(args, extra_args):
     seed = args.seed
 
     learn = get_learn_function(args.alg)
-    alg_kwargs = get_learn_function_defaults(args.alg, env_type)
+    alg_kwargs = get_learn_function_defaults(args.alg, 'mujoco')
     alg_kwargs.update(extra_args)
 
     env = build_env(args)
