6856b0dbd090c512f2164cd0936d2ced6a5d5743
Joe Booth <joe@joebooth.com>
2018-11-08 19:07:37 -0800

    update stable.baselines unit vec env to be closer to baseline version

--- UnityVecEnv.py
+++ UnityVecEnv.py
@@ -4,7 +4,10 @@ import numpy as np
 from gym import spaces
 from stable_baselines.common.vec_env import VecEnv
 from gym_unity.envs import UnityEnv
-
+import time
+import csv
+import os.path as osp
+import json
 
 class UnityVecEnv(VecEnv):
     """
@@ -14,46 +17,70 @@ class UnityVecEnv(VecEnv):
     """
     
     def __init__(self, env_id):
+        print ("**** ", env_id)
         env = UnityEnv(env_id, multiagent=True)
-        self.envs = [fn() for fn in [lambda: env]]
-        # env = self.envs[0]
-        # VecEnv.__init__(self, len(env_fns), env.observation_space, env.action_space)
-        VecEnv.__init__(self, env.number_agents, env.observation_space, env.action_space)
-        shapes, dtypes = {}, {}
-        self.keys = []
+        self.env = env
+        env.num_envs = env.number_agents
+        VecEnv.__init__(self, env.num_envs, env.observation_space, env.action_space)
         obs_space = env.observation_space
-        if isinstance(obs_space, spaces.Dict):
-            assert isinstance(obs_space.spaces, OrderedDict)
-            subspaces = obs_space.spaces
-        else:
-            subspaces = {None: obs_space}
 
-        for key, box in subspaces.items():
-            shapes[key] = box.shape
-            dtypes[key] = box.dtype
-            self.keys.append(key)
-
-        self.buf_obs = {k: np.zeros((self.num_envs,) + tuple(shapes[k]), dtype=dtypes[k]) for k in self.keys}
-        self.buf_dones = np.zeros((self.num_envs,), dtype=np.bool)
-        self.buf_rews = np.zeros((self.num_envs,), dtype=np.float32)
+        # self.keys, shapes, dtypes = obs_space_info(obs_space)
+        # self.buf_obs = { k: np.zeros((self.num_envs,) + tuple(shapes[k]), dtype=dtypes[k]) for k in self.keys }
+        # self.buf_dones = np.zeros((self.num_envs,), dtype=np.bool)
+        # self.buf_rews  = np.zeros((self.num_envs,), dtype=np.float32)
         self.buf_infos = [{} for _ in range(self.num_envs)]
-        self.actions = None
+        # Fake Monitor
+        self.tstart = time.time()
+        self.results_writer = ResultsWriter(
+            "filename",
+            header={"t_start": time.time(), 'env_id' : env.spec and env.spec.id},
+            extra_keys=() + ()
+        )
+        self.reset_keywords = ()
+        self.info_keywords = ()
+        self.allow_early_resets = True
+        self.rewards = None
+        self.needs_reset = True
+        self.episode_rewards = []
+        self.episode_lengths = []
+        self.episode_times = []
+        self.total_steps = 0
+        self.current_reset_info = {} # extra info about the current episode, that was passed in during reset()
 
     def step_async(self, actions):
-        # actions = [self.action_space.sample() for agent in range(self.num_envs)]
-        self.actions = actions.tolist()
+        self.actions = actions
 
     def step_wait(self):
-        obs, rewards, dones, info = self.envs[0].step(self.actions)
-        # self.buf_rews = rewards
-        # self.buf_dones = dones
-        # self.buf_infos = info
+        # obs, self.buf_rews, self.buf_dones, self.buf_infos = self.env.step(action)
+        obs, rews, dones, infos = self.env.step(self.actions.tolist())
+        obs = np.stack(obs)
+        rews = np.stack(rews)
+        dones = np.stack(dones)
+        self.fix_info(infos)
+        self.update(obs[0], rews[0], dones[0], self.buf_infos[0])
+        return (obs, rews, dones, self.buf_infos)
+
+    def update(self, ob, rew, done, info):
+        self.rewards.append(rew)
+        if done:
+            self.needs_reset = True
+            eprew = sum(self.rewards)
+            eplen = len(self.rewards)
+            epinfo = {"r": round(eprew, 6), "l": eplen, "t": round(time.time() - self.tstart, 6)}
+            for k in self.info_keywords:
+                epinfo[k] = info[k]
+            self.episode_rewards.append(eprew)
+            self.episode_lengths.append(eplen)
+            self.episode_times.append(time.time() - self.tstart)
+            epinfo.update(self.current_reset_info)
+            self.results_writer.write_row(epinfo)
+            self.rewards = []
+            self.needs_reset = False
+            if isinstance(info, dict):
+                info['episode'] = epinfo
+
+    def fix_info(self, info):
         for env_idx in range(self.num_envs):
-            self.buf_rews[env_idx] = rewards[env_idx]
-            self.buf_dones[env_idx] = dones[env_idx]
-            # if self.buf_dones[env_idx]:
-                # obs = self.envs[0].reset()[0]
-            self._save_obs(env_idx, obs[env_idx])
             if len(info['brain_info'].action_masks) > 0:
                 self.buf_infos[env_idx]['action_masks'] = info['brain_info'].action_masks[env_idx]
             if len(info['brain_info'].agents) > 0:
@@ -75,35 +102,62 @@ class UnityVecEnv(VecEnv):
             if len(info['brain_info'].vector_observations) > 0:
                 self.buf_infos[env_idx]['vector_observations'] = info['brain_info'].vector_observations[env_idx]
             if len(info['brain_info'].visual_observations) > 0:
-                self.buf_infos[env_idx]['visual_observations'] = info['brain_info'].visual_observations[env_idx]
+                self.buf_infos[env_idx]['visual_observations'] = info['brain_info'].visual_observations[env_idx]    
+        return self.buf_infos    
 
-        return (np.copy(self._obs_from_buf()), np.copy(self.buf_rews), np.copy(self.buf_dones),
-                self.buf_infos.copy())
 
     def reset(self):
-        obs = self.envs[0].reset()
-        for env_idx in range(self.num_envs):
-            self._save_obs(env_idx, obs[env_idx])
-        return np.copy(self._obs_from_buf())
+        self.reset_state()
+        obs = self.env.reset()
+        obs = np.stack(obs)
+        return obs
+
+    def reset_state(self):
+        if not self.allow_early_resets and not self.needs_reset:
+            raise RuntimeError("Tried to reset an environment before done. If you want to allow early resets, wrap your env with Monitor(env, path, allow_early_resets=True)")
+        self.rewards = []
+        self.needs_reset = False
+
+    # def _save_obs(self, e, obs):
+    #     for k in self.keys:
+    #         if k is None:
+    #             self.buf_obs[k][e] = obs
+    #         else:
+    #             self.buf_obs[k][e] = obs[k]
+
+    # def _obs_from_buf(self):
+    #     return dict_to_obs(copy_obs_dict(self.buf_obs))
+
+    def get_images(self):
+        return [self.env.render(mode='rgb_array')]
+
+    def render(self, mode='human'):
+        super().render(mode=mode)
 
     def close(self):
         return
 
-    def get_images(self):
-        return [env.render(mode='rgb_array') for env in self.envs]
-
-    def render(self, *args, **kwargs):
-        return self.envs[0].render(*args, **kwargs)
-
-    def _save_obs(self, env_idx, obs):
-        for key in self.keys:
-            if key is None:
-                self.buf_obs[key][env_idx] = obs
-            else:
-                self.buf_obs[key][env_idx] = obs[key]
-
-    def _obs_from_buf(self):
-        if self.keys == [None]:
-            return self.buf_obs[None]
+class ResultsWriter(object):
+    def __init__(self, filename=None, header='', extra_keys=()):
+        self.extra_keys = extra_keys
+        if filename is None:
+            self.f = None
+            self.logger = None
         else:
-            return self.buf_obs
+            if not filename.endswith("monitor.csv"):
+                if osp.isdir(filename):
+                    filename = osp.join(filename, "monitor.csv")
+                else:
+                    filename = filename + "." + "monitor.csv"
+            self.f = open(filename, "wt")
+            if isinstance(header, dict):
+                header = '# {} \n'.format(json.dumps(header))
+            self.f.write(header)
+            self.logger = csv.DictWriter(self.f, fieldnames=('r', 'l', 't')+tuple(extra_keys))
+            self.logger.writeheader()
+            self.f.flush()
+
+    def write_row(self, epinfo):
+        if self.logger:
+            self.logger.writerow(epinfo)
+            self.f.flush()
